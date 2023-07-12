49518348ac32c74e3095bb5fbf722cf86b218b24
Faheem <faheem@Faheems-MacBook-Pro.local>
2023-07-12 23:29:42 +0530

    add: search functionality

--- src/app/components/LocationList.tsx
+++ src/app/components/LocationList.tsx
@@ -1,104 +1,127 @@
-import { Box, Button, Grid, Stack } from '@mui/material'
-import React, { useEffect, useState } from 'react'
-import './LocationList.css'
-import LoopIcon from '@mui/icons-material/Loop';
-import { gql, useQuery } from '@apollo/client';
-import SearchField from './SearchField';
-import LocationCard from './LocationCard';
-import { FETCH_LOCATIONS } from '../GraphQL/Queries';
+import { Box, Button, Grid, Stack } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import "./LocationList.css";
+import LoopIcon from "@mui/icons-material/Loop";
+import { gql, useQuery } from "@apollo/client";
+import SearchField from "./SearchField";
+import LocationCard from "./LocationCard";
+import { FETCH_LOCATIONS } from "../GraphQL/Queries";
 
 interface Props {
-    setSelectedLocation: React.Dispatch<React.SetStateAction<string>>;
+  setSelectedLocation: React.Dispatch<React.SetStateAction<string>>;
 }
 
 export interface LocationsList {
-    id: string,
-    name: string,
-    status: string,
-    address: string,
-    type: string,
-    updatedAt: number,
+  id: string;
+  name: string;
+  status: string;
+  address: string;
+  type: string;
+  updatedAt: number;
 }
 
 const LocationList: React.FC<Props> = ({ setSelectedLocation }) => {
+  const [page, setPage] = useState<number>(0);
+  const [limit, setLimit] = useState<number>(5);
+  const [locationList, setLocationList] = useState<LocationsList[]>();
+  const [searchText, setSearchText] = useState<string>("");
 
-    const [page, setPage] = useState<number>(0)
-    const [limit, setLimit] = useState<number>(5)
-    const [locationList, setLocationList] = useState<LocationsList[]>()
+  const { error, loading, data, fetchMore } = useQuery(FETCH_LOCATIONS, {
+    variables: {
+      tenant: "692627ef-fda8-4203-b108-e8e9f52ad410",
+      page: page,
+      limit: limit,
+    },
+  });
 
-    const { error, loading, data, fetchMore } = useQuery(FETCH_LOCATIONS, {
-        variables: {
-            tenant: "692627ef-fda8-4203-b108-e8e9f52ad410",
-            page: page,
-            limit: limit,
-        }
-
+  useEffect(() => {
+    fetchMore({
+      variables: {
+        page,
+        limit,
+      },
     });
+  }, [page, limit]);
 
-    useEffect(() => {
-        fetchMore({
-            variables: {
-                page,
-                limit,
-            }
-        });
+  useEffect(() => {
+    if (data?.locationList?.resources?.[0]) {
+      if (searchText) {
+        setLocationList(
+          data?.locationList?.resources?.filter((item: LocationsList) =>
+            item?.name.includes(searchText)
+          )
+        );
+      } else {
+        setLocationList(data?.locationList?.resources);
+      }
+    }
+  }, [data, searchText]);
 
-    }, [page, limit])
+  if (loading) return <span>Loading...</span>;
 
-    useEffect(() => {
-        if (data?.locationList?.resources?.[0]) {
-            setLocationList(data?.locationList?.resources)
-        }
-    }, [data])
+  if (error) return <p>Oops! {error?.message}</p>;
 
-    if (loading) return <span>Loading...</span>;
-    if (error) return <p>Oops! {error?.message}</p>;
-    return (
-        <Grid item container xs={12} className='list_container'>
-            <Grid item container xs={12} alignItems="center">
-                <Stack direction="row" alignItems="center" width="100%">
-                    <Button variant="outlined" fullWidth={true} size='small' className='refresh_btn'>
-                        <LoopIcon />
-                    </Button>
-                    <Box className="heading" onClick={() => setLimit(6)}>
-                        Locations
-                    </Box>
-                </Stack>
-            </Grid>
-            <Grid item container xs={12}
-                mt={2}
-                mb={2}
+  return (
+    <Grid item container xs={12} className="list_container">
+      <Grid item container xs={12} alignItems="center">
+        <Stack direction="row" alignItems="center" width="100%">
+          <Button
+            variant="outlined"
+            fullWidth={true}
+            size="small"
+            className="refresh_btn"
+          >
+            <LoopIcon />
+          </Button>
+          <Box className="heading" onClick={() => setLimit(6)}>
+            Locations
+          </Box>
+        </Stack>
+      </Grid>
+      <Grid item container xs={12} mt={2} mb={2}>
+        <SearchField searchText={searchText} setSearchText={setSearchText} />
+      </Grid>
+
+      <Grid item container xs={12}>
+        <Stack
+          direction="row"
+          spacing={2}
+          sx={{
+            maxWidth: "100%",
+            overflowX: "scroll",
+            pb: 2,
+          }}
+        >
+          {[1, 2, 3, 4, 5, 6, 7].map((item) => (
+            <Button
+              variant="outlined"
+              size="small"
+              sx={{
+                minWidth: "80px",
+              }}
             >
-                <SearchField />
-            </Grid>
+              Filter {item}
+            </Button>
+          ))}
+        </Stack>
+      </Grid>
 
-            <Grid item container xs={12}>
-                <Stack direction="row" spacing={2} sx={{
-                    maxWidth: "100%",
-                    overflowX: "scroll",
-                    pb: 2
-                }}>
-                    {
-                        [1, 2, 3, 4, 5, 6, 7].map((item) => (
-                            <Button variant='outlined' size="small" sx={{
-                                minWidth: "80px"
-                            }}>Filter {item}</Button>
-                        ))
-                    }
-                </Stack>
-            </Grid>
+      <Grid item container xs={12} mb={2}>
+        {" "}
+        {locationList &&
+          locationList?.map((locData) => (
+            <LocationCard
+              locData={locData}
+              setSelectedLocation={setSelectedLocation}
+            />
+          ))}
+      </Grid>
+      <Grid item container xs={12}>
+        {" "}
+        Pagination
+      </Grid>
+    </Grid>
+  );
+};
 
-            <Grid item container xs={12}
-                mb={2}> {
-                    locationList && locationList?.map((locData) => (
-                        <LocationCard locData={locData} setSelectedLocation={setSelectedLocation} />
-                    ))
-                }</Grid>
-            <Grid item container xs={12}> Pagination</Grid>
-
-
-        </Grid>
-    )
-}
-
-export default LocationList
+export default LocationList;
--- src/app/components/SearchField.tsx
+++ src/app/components/SearchField.tsx
@@ -1,66 +1,66 @@
-import SearchIcon from '@mui/icons-material/Search';
-import Box from '@mui/material/Box';
-import InputBase from '@mui/material/InputBase';
-import Typography from '@mui/material/Typography';
-import { alpha, styled } from '@mui/material/styles';
+import SearchIcon from "@mui/icons-material/Search";
+import Box from "@mui/material/Box";
+import InputBase from "@mui/material/InputBase";
+import Typography from "@mui/material/Typography";
+import { alpha, styled } from "@mui/material/styles";
+import React from "react";
 
-const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    border: "1px solid #ccc",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-        backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: '100%',
-
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  border: "1px solid #ccc",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
+  color: "inherit",
+  width: "100%",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
     width: "100%",
-    '& .MuiInputBase-input': {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-
-    },
+  },
 }));
 
-export default function SearchField() {
-
-
-
-
-
-
-
-    return (
-        <Box sx={{ width: "100%" }}>
-            <Search>
-                <SearchIconWrapper>
-                    <SearchIcon />
-                </SearchIconWrapper>
-                <StyledInputBase
-                    placeholder="Search…"
-                    inputProps={{ 'aria-label': 'search' }}
-                />
-            </Search>
-
-        </Box>
-    );
+interface Props {
+  searchText: string;
+  setSearchText: React.Dispatch<React.SetStateAction<string>>;
 }
+
+const SearchField: React.FC<Props> = ({ searchText, setSearchText }) => {
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Search>
+        <SearchIconWrapper>
+          <SearchIcon />
+        </SearchIconWrapper>
+        <StyledInputBase
+          placeholder="Search…"
+          inputProps={{ "aria-label": "search" }}
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+      </Search>
+    </Box>
+  );
+};
+
+export default SearchField;
